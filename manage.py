e08b025863ae20e7ef26fed16273b4656b9636cf
gentlepuck071 <163500180+gentlepuck071@users.noreply.github.com>
2024-04-30 15:39:28 +0900

    Update manage.py
    fix

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ def main():
       
 
     try:
-        from django.core.management import execute_from_command_line  
+        from django.core.management import execute_from_command_line     
     except ImportError as exc:    
         raise ImportError(   
             "Couldn't import Django. Are you sure it's installed and "
