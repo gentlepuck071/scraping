371e5cf8833dd2cff963dea2882631a96afc7249
gentlepuck071 <163500180+gentlepuck071@users.noreply.github.com>
2024-04-30 16:55:48 +0900

    Update manage.py
    fix

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')   
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')      
       
 
     try:
